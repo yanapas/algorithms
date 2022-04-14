f0a3a38af404c5076ecad8dac5478382e062c3db
Iana Pastukhova <monstervan221188@gmail.com>
2022-04-14 14:27:54 -0700

    add fizzBuzz

---
+++ fizzBazz/fizzBazz.js
@@ -0,0 +1,17 @@
+//return arr
+function fizzBazz(n){
+  let arr = []
+  for(let i = 1; i <= n; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+      arr.push('FizzBuzz')
+    } else if (i % 3 === 0){
+      arr.push('Fizz')
+    } else if (i % 5 === 0){
+      arr.push('Buzz')
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr.join(', ')
+}
+console.log(fizzBazz(15));
